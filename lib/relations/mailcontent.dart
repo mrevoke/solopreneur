2156b331e1d2ca076c9751b9224294fd45328426
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2024-02-07 06:58:44 +0530

    added emailjs

--- lib/relations/mailcontent.dart
+++ lib/relations/mailcontent.dart
@@ -6,13 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
 import 'package:http/http.dart' as http;
 
 class MailContent extends StatefulWidget {
   @override
   _MailContentState createState() => _MailContentState();
 }
+
 
 class _MailContentState extends State<MailContent> {
   TextEditingController _questionController = TextEditingController();
@@ -45,6 +45,39 @@ class _MailContentState extends State<MailContent> {
     }
   }
 
+  Future sendEmail(String name, String email,String emailContent) async {
+  final url = Uri.parse("https://api.emailjs.com/api/v1.0/email/send");
+  const serviceId="service_crxu9n9";
+  const templateId="template_9jnm1ni";
+  const userId="JZIkQgu9SDh1isUcJ";
+  List<String> lines = emailContent.split('\n');
+  String subject = '';
+String remainingContent = '';
+for (int i = 0; i < lines.length; i++) {
+  if (lines[i].trim().startsWith('Subject:')) {
+    subject = lines[i].trim().substring('Subject:'.length).trim();
+    remainingContent = lines.sublist(i + 1).join('\n').trim();
+    break;
+  }
+}
+  final response= await http.post(url,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: json.encode({
+        "service_id": serviceId,
+        "template_id": templateId,
+        "user_id": userId,
+        "template_params": {
+          "name": name,
+          "email": email,
+          "subject": subject,
+          "message": remainingContent,
+        }
+      })
+  );
+  return response.statusCode;
+}
 
   @override
   var t=false;
@@ -98,7 +131,7 @@ class _MailContentState extends State<MailContent> {
                         if (question.isNotEmpty) {
                           question="Hey Chat GPT help me write a cold mail in less than 400 word proper format"
                               "and in an eye catchy manner to get reply from my investor while "
-                              "keeping these things also in mind"+question;
+                              "keeping these things also in mind"+question+" also include subject with email";
                           askGPT(question);
                           _questionController.clear();
                         }
@@ -176,15 +209,46 @@ class _MailContentState extends State<MailContent> {
   }
 
   void _showCardDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          content: Card(
-            child: Text('chats'),
+  String email = '';
+  String name = '';
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Send Email'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+               onSubmitted: (value) => email = value,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Name'),
+               onSubmitted: (value) => name = value,
+            ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context); 
+              sendEmail(name,email,resp); 
+            },
+            child: Text('Send'),
           ),
-        );
-      },
-    );
-  }
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Cancel'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+ 
 }
