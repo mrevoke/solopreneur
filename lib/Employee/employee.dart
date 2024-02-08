a85dc678fed7b6c436d2d81e192f43fe86f3bc0c
Jeet Singh <jeet29113@gmail.com>
2024-02-09 00:23:22 +0530

    storing employee data

--- lib/Employee/employee.dart
+++ lib/Employee/employee.dart
@@ -1,134 +1,203 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:solopreneuer/Employee/labeltext.dart';
-import 'package:solopreneuer/relations/mailsection.dart';
-
-import '../relations/chatsection.dart';
+import 'package:solopreneuer/relations/chatsection.dart';
 
 class employee extends StatefulWidget {
-  const employee({super.key});
+  const employee({Key? key}) : super(key: key);
 
   @override
   State<employee> createState() => _employeeState();
 }
 
 class _employeeState extends State<employee> {
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController dobController = TextEditingController();
+  final TextEditingController profileLinksController = TextEditingController();
+  final TextEditingController relocateController = TextEditingController();
+  final TextEditingController locationController = TextEditingController();
+  final TextEditingController salaryController = TextEditingController();
+  final TextEditingController experienceController = TextEditingController();
+  final TextEditingController skillsController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController currentCompanyController =
+      TextEditingController();
+  final TextEditingController graduationDateController =
+      TextEditingController();
+  final TextEditingController profileLink2Controller = TextEditingController();
+  final TextEditingController haveVisaController = TextEditingController();
+  final TextEditingController currentLocationController =
+      TextEditingController();
+  final TextEditingController expectedSalaryController =
+      TextEditingController();
+  final TextEditingController genderController = TextEditingController();
+  final TextEditingController noticePeriodController = TextEditingController();
+
+  Future<void> saveDataToFirebase() async {
+    try {
+      String userId = FirebaseAuth.instance.currentUser?.uid ?? "";
+      CollectionReference employeeCollection =
+          FirebaseFirestore.instance.collection('employees');
+
+      await employeeCollection.doc(userId).set({
+        'fullName': fullNameController.text,
+        'phoneNumber': phoneNumberController.text,
+        'dob': dobController.text,
+        'profileLinks': profileLinksController.text,
+        'relocate': relocateController.text,
+        'location': locationController.text,
+        'salary': salaryController.text,
+        'experience': experienceController.text,
+        'skills': skillsController.text,
+        'email': emailController.text,
+        'currentCompany': currentCompanyController.text,
+        'graduationDate': graduationDateController.text,
+        'profileLink2': profileLink2Controller.text,
+        'haveVisa': haveVisaController.text,
+        'currentLocation': currentLocationController.text,
+        'expectedSalary': expectedSalaryController.text,
+        'gender': genderController.text,
+        'noticePeriod': noticePeriodController.text,
+      });
+
+      print('Data saved to Firestore');
+    } catch (error) {
+      print('Error saving data to Firestore: $error');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration:BoxDecoration(
-        gradient: LinearGradient(
-          colors: [const Color.fromARGB(255, 238, 238, 238)!, Colors.white], // Adjust colors as needed
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-     // color: Color.fromARGB(255, 226, 241, 247),
-      width: MediaQuery.sizeOf(context).width,
-      child: Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  //MailContent(),expande
-                  
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                     SizedBox(height:MediaQuery.of(context).size.height *0.055 ,),
-
-                    
-                      const LabeledTextField(labelText: "Full Name", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Phone Number", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "DOB", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Profile Links", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Ready To Relocate", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Preffered Location", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Current Salary", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Experience", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Skills", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                  
-                    ],
-                  ),        Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                     SizedBox(height:MediaQuery.of(context).size.height *0.055 ,),
-    
-                      const LabeledTextField(labelText: "Email", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Current Company", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Graduation Date", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Profile Link 2", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Have Visa", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Current Location", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Expected Salary", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Gender", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                      const LabeledTextField(labelText: "Notice Period", hintText: "Type here"),
-                     SizedBox(height:MediaQuery.of(context).size.height *0.022 ,),
-                  
-                    ],
-                  ), 
-            ///////add herecontain
-            Container(height: MediaQuery.sizeOf(context).height*0.9,
-      decoration: BoxDecoration(
-        color: Colors.white, // Set your desired background color
-        borderRadius: BorderRadius.circular(20.0),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.grey,
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: Offset(0, 3),
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [const Color.fromARGB(255, 238, 238, 238)!, Colors.white],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
-        ],
-      ),
-      child: Container(alignment: Alignment.center,//height:MediaQuery.of(context).size.height * 4/7 ,
-      width: MediaQuery.of(context).size.width * 2/7,
-        child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //crossAxisAlignment: CrossAxisAlignment.start,
-           children: [
-            SizedBox(height:MediaQuery.of(context).size.height*0.15/7 ,),
-         Text("Frequent Chats",style: TextStyle(fontSize:MediaQuery.of(context).size.height*0.25/7,fontWeight: FontWeight.bold ),),
-            SizedBox(height:MediaQuery.of(context).size.height*0.15/7 ,),
-          ChatSection(recipientName: "NYC client", subject: "Payment due"),
-            SizedBox(height:MediaQuery.of(context).size.height*0.09/7 ,),
-         
-          ChatSection(recipientName: "Supplier", subject: "Updates"),
-            SizedBox(height:MediaQuery.of(context).size.height*0.09/7 ,),
-          ChatSection(recipientName: "Investor", subject: "investments!"),
-          //ChatSection(recipientName: "NYC client", subject: "Payment due"),
-            SizedBox(height:MediaQuery.of(context).size.height*0.09/7 ,),
-          ChatSection(recipientName: "U.S client", subject: "Enquiry"),
-            SizedBox(height:MediaQuery.of(context).size.height*0.09/7 ,),
-          ChatSection(recipientName: "Team A", subject: "Progress"),
-            SizedBox(height:MediaQuery.of(context).size.height*0.09/7 ,),
-          ChatSection(recipientName: "Team B", subject: "Progress"),
-            SizedBox(height:MediaQuery.of(context).size.height*0.09/7 ,),
-          ChatSection(recipientName: "NYC client", subject: "Payments done"),
-            SizedBox(height:MediaQuery.of(context).size.height*0.09/7 ,),
-          ChatSection(recipientName: "U.S client", subject: "Business eqry"),
-            SizedBox(height:MediaQuery.of(context).size.height*0.2/7 ,),
-      
-        ],
+        ),
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // First Column
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.055),
+                LabeledTextField(
+                    labelText: "Full Name",
+                    hintText: "Type here",
+                    controller: fullNameController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Phone Number",
+                    hintText: "Type here",
+                    controller: phoneNumberController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "DOB",
+                    hintText: "Type here",
+                    controller: dobController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Profile Links",
+                    hintText: "Type here",
+                    controller: profileLinksController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Ready To Relocate",
+                    hintText: "Type here",
+                    controller: relocateController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Preffered Location",
+                    hintText: "Type here",
+                    controller: locationController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Current Salary",
+                    hintText: "Type here",
+                    controller: salaryController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Experience",
+                    hintText: "Type here",
+                    controller: experienceController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Skills",
+                    hintText: "Type here",
+                    controller: skillsController),
+              ],
+            ),
+
+            // Second Column
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.055),
+                LabeledTextField(
+                    labelText: "Email",
+                    hintText: "Type here",
+                    controller: emailController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Current Company",
+                    hintText: "Type here",
+                    controller: currentCompanyController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Graduation Date",
+                    hintText: "Type here",
+                    controller: graduationDateController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Profile Link 2",
+                    hintText: "Type here",
+                    controller: profileLink2Controller),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Have Visa",
+                    hintText: "Type here",
+                    controller: haveVisaController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Current Location",
+                    hintText: "Type here",
+                    controller: currentLocationController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Expected Salary",
+                    hintText: "Type here",
+                    controller: expectedSalaryController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Gender",
+                    hintText: "Type here",
+                    controller: genderController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                LabeledTextField(
+                    labelText: "Notice Period",
+                    hintText: "Type here",
+                    controller: noticePeriodController),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.022),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      saveDataToFirebase();
+                    },
+                    child: Text('Save to Firebase'),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
-    )
-    
-                ],
-              ),
     );
   }
 }
--- lib/Employee/labeltext.dart
+++ lib/Employee/labeltext.dart
@@ -3,33 +3,34 @@ import 'package:flutter/material.dart';
 class LabeledTextField extends StatelessWidget {
   final String labelText;
   final String hintText;
+  final TextEditingController controller;
 
   const LabeledTextField({
     Key? key,
     required this.labelText,
     required this.hintText,
+    required this.controller,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double containerHeight = MediaQuery.of(context).size.height * 0.18/3;
+    double containerHeight = MediaQuery.of(context).size.height * 0.18 / 3;
 
-    return Container(color: Color.fromARGB(255, 251, 254, 255),
+    return Container(
+      color: Color.fromARGB(255, 251, 254, 255),
       width: MediaQuery.of(context).size.width * 0.23,
       height: containerHeight,
-      // padding: EdgeInsets.all(8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          
-          Container(height: containerHeight,
+          Container(
+            height: containerHeight,
             child: TextFormField(
-              
-              controller: TextEditingController(),
+              controller: controller,
               decoration: InputDecoration(
                 hintText: hintText,
                 hintStyle: TextStyle(
-                  fontSize: containerHeight / 10*3, // Adjust as needed
+                  fontSize: containerHeight / 10 * 3,
                 ),
                 labelText: labelText,
                 border: OutlineInputBorder(),
